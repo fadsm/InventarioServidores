ace4c0ee9d60f5f63289d5318c69f9114f2ba9a5
fadsm <fadsm@github.com>
2018-06-18 20:44:14 -0300

    Implementando interfaces e gerando controllers

---
+++ Modelo/ClassLibrary2/BASE/IBaseController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controllers.BASE
+{
+    interface IBaseController<T> where T : class
+    {
+        void Adicionar(T entity);
+        IList<T> ListarTodos();
+
+        IList<T> ListarPorNome(string nome);
+
+        T BuscarPorID(int id);
+
+        void Editar(T entity);
+
+        void Excluir(int id);
+    }
+}
---
+++ Modelo/ClassLibrary2/MachineController.cs
@@ -0,0 +1,40 @@
+﻿using Controllers.BASE;
+using System;
+using System.Collections.Generic;
+using Model;
+
+namespace Controllers
+{
+    public class MachineController : IBaseController<Machine>
+    {
+        public void Adicionar(Machine entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Machine BuscarPorID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Editar(Machine entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Machine> ListarPorNome(string nome)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Machine> ListarTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
