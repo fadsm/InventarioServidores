5b352d91219eb4163472821cd275770ffde1cebb
fadsm <fadsm@github.com>
2018-06-18 19:33:33 -0300

    Atualizando os modelos e corrigindo sintaxe

---
+++ InventarioServer/Model/Machine.cs
@@ -0,0 +1,24 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Machine
+    {
+        public int MachineID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Ip { get; set; }
+
+        public string OperatingSystem { get; set; }
+
+        public string Manufacturer { get; set; }
+
+        public virtual Cluster _Cluster {get; set;}
+    }
+}
