910ee946cd23f8193defa4206a91a7796f733808
fadsm <fadsm@github.com>
2018-06-18 21:31:27 -0300

    Implementing machine add

---
+++ Modelo/ClassLibrary2/DAL/Context.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.Entity;
+using Model;
+
+namespace Controllers.DAL
+{
+    class Context : DbContext
+    {
+        public Context() : base("strConn")
+        {
+            // Padrao (se nao existir base de dados, cria)
+            //Database.SetInitializer(new CreateDatabaseIfNotExists<Contexto>());
+
+            // Apaga e recria a base toda vez que o projeto eh executado
+            //Database.SetInitializer(new DropCreateDatabaseAlways<Contexto>());
+
+            // Apaga e recria a base de dados se houver alteracoes nas modelos
+            //Database.SetInitializer(new DropCreateDatabaseIfModelChanges<Contexto>());
+        }
+
+        public DbSet<Machine> Machines { get; set; }
+        public DbSet<Cluster> Clusters { get; set; }
+        public DbSet<Datacenter> Datacenters { get; set; }
+
+    }
+}
--- Modelo/ClassLibrary2/MachineController.cs
+++ Modelo/ClassLibrary2/MachineController.cs
@@ -2,37 +2,42 @@
 using System;
 using System.Collections.Generic;
 using Model;
+using Controllers.DAL;
+
 
 namespace Controllers
 {
     public class MachineController : IBaseController<Machine>
     {
-        public void Adicionar(Machine entity)
+        private Context context = new Context();
+
+        public void Add(Machine entity)
+        {
+            context.Machines.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Delete(int id)
         {
             throw new NotImplementedException();
         }
 
-        public Machine BuscarPorID(int id)
+        public void Edit(Machine entity)
         {
             throw new NotImplementedException();
         }
 
-        public void Editar(Machine entity)
+        public IList<Machine> ListAll()
         {
             throw new NotImplementedException();
         }
 
-        public void Excluir(int id)
+        public IList<Machine> ListByName(string name)
         {
             throw new NotImplementedException();
         }
 
-        public IList<Machine> ListarPorNome(string nome)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IList<Machine> ListarTodos()
+        public Machine SearchForId(int id)
         {
             throw new NotImplementedException();
         }
